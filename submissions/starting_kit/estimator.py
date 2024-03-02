67e69229838a024d98535d84cf4ae657cdd3963e
ClementTeuteu <154089221+ClementTeuteu@users.noreply.github.com>
2024-03-02 18:46:24 +0100

    Update estimator.py

--- submissions/starting_kit/estimator.py
+++ submissions/starting_kit/estimator.py
@@ -2,5 +2,7 @@ from sklearn.ensemble import RandomForestRegressor
 
 
 def get_estimator():
-    reg = RandomForestRegressor()
+    reg = RandomForestRegressor(
+        n_estimators=2, max_leaf_nodes=2, random_state=61
+    )
     return reg
