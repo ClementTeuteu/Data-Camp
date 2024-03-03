19086b0506e762ae43ca09bd54aef26cafd2fa79
ClementTeuteu <154089221+ClementTeuteu@users.noreply.github.com>
2024-03-03 10:29:02 +0100

    Update estimator.py

--- submissions/starting_kit/estimator.py
+++ submissions/starting_kit/estimator.py
@@ -1,8 +1,17 @@
+from sklearn.pipeline import make_pipeline, Pipeline
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 
-
 def get_estimator():
-    reg = RandomForestRegressor(
-        n_estimators=20, max_leaf_nodes=2, random_state=61
+    base_transformer= Pipeline(steps=[
+            ("imputer", IterativeImputer(max_iter=10, random_state=42)),  
+            ("scaler", StandardScaler())  
+        ])
+    
+    reg = Pipeline(
+        steps=[("transformer", base_transformer), ("regressor", RandomForestRegressor(
+            n_estimators=20, max_leaf_nodes=2, random_state=61))]
     )
     return reg
